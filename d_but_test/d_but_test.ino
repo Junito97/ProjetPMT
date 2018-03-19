02aeac60ec770def8e39caeec3eec297345c72bf
Paul <PaulJohnson@Johnson.com>
2018-03-19 15:35:38 -0500

    code fonctionnel :)

--- d_but_test/d_but_test.ino
+++ d_but_test/d_but_test.ino
@@ -11,33 +11,26 @@ ESP8266WiFiMulti wifiMulti;
 void setup() {
     Serial.begin(115200);
     delay(10);
-	
+  
     WiFi.mode(WIFI_STA);
-    wifiMulti.addAP("PaulJohnson", "pluriels2");
+    wifiMulti.addAP("Paul Johnson", "pluriels2");
     wifiMulti.addAP("iPhone de Jonathan", "fgwk9u5hgggs7");
-    wifiMulti.addAP("NETGEAR", "v13btsiris");
+    wifiMulti.addAP("NETGEAR", "v13btsiri");
 
-	Serial.println("Connecting Wifi...");
+    Serial.println("Connecting Wifi...");
+    int i = 0;
+    while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect: scan for Wi-Fi networks, and connect to the strongest of the networks above
+        delay(1000);
+        Serial.print('.');
+    }
     if(wifiMulti.run() == WL_CONNECTED) {
         Serial.println("");
         Serial.println("WiFi connected");
         Serial.println("IP address: ");
         Serial.println(WiFi.localIP());
-        
-    }
-      Serial.print("");
-      delay(5000);
-      Serial.println("AT+CIPSTATUS=id,type,addr,port,tetype");
+        Serial.print("Connecter au réseau wifitte :");
+        Serial.println(WiFi.SSID());
+        Serial.println("Welcome :)");
+  }
 }
-
-void loop() {
-    if(wifiMulti.run() != WL_CONNECTED) {
-        Serial.println("WiFi not connected!");
-        delay(1000);
-    }
-    else {
-    Serial.print("");
-      delay(500);
-      Serial.println("AT+CIPSTATUS=id,type,addr,port,tetype");
-}
-}
+void loop(){}
